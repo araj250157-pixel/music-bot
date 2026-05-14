f3c3b2f14d704ea8504c038e77837c77ec140682
araj250157-pixel <araj250157@gmail.com>
2026-05-14 23:13:34 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import yt_dlp
+from flask import Flask
+from threading import Thread
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def download_song(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.message.text
+    await update.message.reply_text("🔍 Searching...")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'noplaylist': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f"ytsearch:{query}", download=True)['entries'][0]
+        file_name = ydl.prepare_filename(info)
+
+    await update.message.reply_audio(audio=open(file_name, 'rb'))
+
+app_flask = Flask('')
+
+@app_flask.route('/')
+def home():
+    return "Bot running"
+
+def run():
+    app_flask.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    Thread(target=run).start()
+
+keep_alive()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download_song))
+app.run_polling()
